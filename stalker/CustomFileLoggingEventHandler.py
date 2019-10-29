9826e589ab4bae7098625e81b98af22b0dd10f01
thanh tan Nguyen <thanhtan004100@gmail.com>
2019-10-29 23:27:22 +0700

    [Update] Logging

--- stalker/CustomFileLoggingEventHandler.py
+++ stalker/CustomFileLoggingEventHandler.py
@@ -7,13 +7,6 @@ from watchdog.events import FileSystemEventHandler
 class LoggingEventHandler(FileSystemEventHandler):
     """Logs all the events captured."""
 
-    def on_moved(self, event):
-        super(LoggingEventHandler, self).on_moved(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Moved %s: from %s to %s", what, event.src_path,
-                     event.dest_path)
-
     def on_created(self, event):
         super(LoggingEventHandler, self).on_created(event)
 
@@ -21,17 +14,12 @@ class LoggingEventHandler(FileSystemEventHandler):
         total_lines = self.analyze_file(src_path=event.src_path)
         logging.info("Created : %s", event.src_path)
 
-    def on_deleted(self, event):
-        super(LoggingEventHandler, self).on_deleted(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Deleted %s: %s", what, event.src_path)
-
     def on_modified(self, event):
         super(LoggingEventHandler, self).on_modified(event)
 
         what = 'directory' if event.is_directory else 'file'
         logging.info("Modified %s: %s", what, event.src_path)
+        logging.info("===" * 7 + "End" + "===" * 7)
 
     def analyze_file(self, src_path):
         full_path = Path(src_path).absolute()
@@ -40,9 +28,16 @@ class LoggingEventHandler(FileSystemEventHandler):
             line_number = 0
             for line in lines:
                 line_number  = line_number + 1
-                trailing_char = line[-2]
-                print(line.count())
-                if not trailing_char == ";":
-                    logging.info("Line %d: %s", line_number, "missing trailing char \";\" at the end")
+                if len(line) > 1:
+                    trailing_char = line[-2]
+                    if not trailing_char == ";":
+                        logging.info("Line %d: %s", line_number, "missing trailing char \";\" at the end")
+                
+                check_static_call = line.find(":")
+                print(check_static_call)
+                print(line[check_static_call + 1])
+                if check_static_call > -1 
+                    and check_static_call != line[check_static_call + 1]:
+                    logging.info("Line %d: %s", line_number, "missing colon char \":\" for static call")
         return lines
 
